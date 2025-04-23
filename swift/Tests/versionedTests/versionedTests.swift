9f5603d166bd606351110a1616682ca2cbf74e55
Rene Rivera <grafikrobot@gmail.com>
2025-04-22 20:50:57 -0500

    Add empty "C" shell. And add some missing (c) comments.

--- swift/Tests/versionedTests/versionedTests.swift
+++ swift/Tests/versionedTests/versionedTests.swift
@@ -1,3 +1,7 @@
+// Copyright René Ferdinand Rivera Morell
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 import Testing
 @testable import versioned
 
